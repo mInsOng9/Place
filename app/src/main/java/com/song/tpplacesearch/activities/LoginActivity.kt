8f189f7f9f01ac34cfb219fe2a48734984548711
song <minns2992@gmail.com>
2022-11-30 16:30:04 +0900

    intro and login

--- app/src/main/java/com/song/tpplacesearch/activities/LoginActivity.kt
+++ app/src/main/java/com/song/tpplacesearch/activities/LoginActivity.kt
@@ -1,11 +1,49 @@
 package com.song.tpplacesearch.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.song.tpplacesearch.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    val binding:ActivityLoginBinding by lazy { ActivityLoginBinding.inflate(LayoutInflater.from(this)) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        // 둘러보기 글씨 클릭으로 로그인 없이 Main 화면으로 이동
+        binding.tvGo.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+        //회원가입 버튼 클릭
+        binding.tvSignup.setOnClickListener {
+            startActivity(Intent(this,SignupActivity::class.java))
+        }
+
+        //이메일 로그인 버튼 클릭
+        binding.layoutEmail.setOnClickListener {
+            startActivity(Intent(this,EmailLoginActivity::class.java))
+        }
+
+        //간편로그인 버튼들
+        binding.btnLoginKakao.setOnClickListener { clickedLoginKakao() }
+        binding.btnLoginGoogle.setOnClickListener { clickedLoginGoogle() }
+        binding.btnLoginNaver.setOnClickListener { clickedLoginNaver() }
+
+
+    }
+    fun clickedLoginKakao(){
+        Toast.makeText(this, "kakao", Toast.LENGTH_SHORT).show()
+    }
+    fun clickedLoginGoogle(){
+        Toast.makeText(this, "google", Toast.LENGTH_SHORT).show()
+    }
+    fun clickedLoginNaver(){
+        Toast.makeText(this, "naver", Toast.LENGTH_SHORT).show()
+
     }
 }
